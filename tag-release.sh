a5d13d1ff94d4f187a740a61de18bee9dd053586
Jaime Garcia Ramirez <jgarciar@vt.edu>
2015-06-19 10:41:19 -0400

    [CONF] - Updated tag release script to display when a release will not be tagged

--- tag-release.sh
+++ tag-release.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [ "${TRAVIS_PULL_REQUEST}" -eq "false" ] && [ "${TRAVIS_BRANCH}" -eq "master" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
 	echo "Tagging release as $GIT_TAG"
   	git config --global user.email "builds@travis-ci.com"
   	git config --global user.name "Travis CI"
   	git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
   	git push --quiet https://$TRAVIS_PERMISSIONS@github.com/BridgePhase/ctf $GIT_TAG > /dev/null 2>&1
+else 
+	echo "Not tagging this release because branch ${TRAVIS_BRANCH} is not master"
 fi
