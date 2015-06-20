e4ed8b051ac436999de7787a775d660169ec2979
Jaime Garcia <garciar.jaime@gmail.com>
2015-06-20 19:42:16 -0400

    #17 - adding Gulp support for javascript files

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var debug = require('gulp-debug');
+var uglify = require('gulp-uglifyjs');
+
+
+gulp.task('uglify', function() {
+  gulp.src([	 
+	  'src/main/resources/public/libraries/angular/angular.min.js',
+	   'src/main/resources/public/libraries/angular-ui-router/release/angular-ui-router.min.js',
+	  'src/main/resources/public/modules/*.js',
+	  'src/main/resources/public/modules/services/**/*.js',
+	  'src/main/resources/public/modules/directives/**/*.js',
+	  'src/main/resources/public/modules/controllers/**/*.js'
+	])
+	.pipe(debug())
+	.pipe(uglify('ctf-app.js'))
+    .pipe(gulp.dest('src/main/resources/public/'));
+});
+
+gulp.task('concatenate', function() {
+	
+})
+
+gulp.task('default', function() {
+});
