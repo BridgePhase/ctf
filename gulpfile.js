f522dcc3d4cf794c453b4e6e3878a1ab46ebc1a0
Jaime Garcia <garciar.jaime@gmail.com>
2015-06-20 21:31:52 -0400

    #17 - Fixing ordering of gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,51 +3,54 @@ var debug = require('gulp-debug');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var uglifyCss = require('gulp-uglifycss');
+var runSequence = require('run-sequence');
 
-gulp.task('uglify', function() {
-  gulp.src([	 
+gulp.task('uglifyJs', function() {
+  return gulp.src([	 
 	  'src/main/resources/public/modules/*.js',
 	  'src/main/resources/public/modules/services/**/*.js',
 	  'src/main/resources/public/modules/directives/**/*.js',
 	  'src/main/resources/public/modules/controllers/**/*.js'
 	])
-	.pipe(debug())
+	.pipe(debug({title: 'uglifyjs'}))
 	.pipe(uglify())
 	.pipe(concat('ctf-app-module.js'))
 	.pipe(gulp.dest('src/main/resources/public'));
 });
 
 gulp.task('concatenateJs', function() {
-	gulp.src([
+	return gulp.src([
 		'src/main/resources/public/libraries/angular/angular.min.js',
 		'src/main/resources/public/libraries/angular-ui-router/release/angular-ui-router.min.js',
 		'src/main/resources/public/ctf-app-module.js'
 	])
-	.pipe(debug())
+	.pipe(debug({title: 'concatenatejs'}))
 	.pipe(concat('ctf-app.js'))
 	.pipe(gulp.dest('src/main/resources/public'));
 });
 
 gulp.task('uglifyCss', function() {
-  gulp.src([	 
+  return gulp.src([	 
 		'src/main/resources/public/styles/bridgephase.css'
 	])
-	.pipe(debug())
+	.pipe(debug({title:'minifyCss'}))
 	.pipe(uglifyCss())
 	.pipe(concat('bridgephase.min.css'))
 	.pipe(gulp.dest('src/main/resources/public/styles/'));
 });
 
-gulp.task('concatenateCss', ['uglifyCss'], function() {
-	gulp.src([
+gulp.task('concatenateCss', function() {
+	return gulp.src([
 		'src/main/resources/public/styles/pure-min.css',
 		'src/main/resources/public/styles/grids-responsive-min.css',
 		'src/main/resources/public/styles/bridgephase.min.css'
 	])
-	.pipe(debug())
+	.pipe(debug({title:'concatenateCss'}))
 	.pipe(concat('ctf-styles.css'))
 	.pipe(gulp.dest('src/main/resources/public'));
 });
 
-gulp.task('default', ['uglify', 'concatenateJs', 'concatenateCss'], function() {
+gulp.task('default', function() {
+	runSequence('uglifyCss', 'concatenateCss');
+	runSequence('uglifyJs', 'concatenateJs');
 });
