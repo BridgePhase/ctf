c5cc3ec330524c0c3c7bd3159da51555303d0269
Jaime Garcia <garciar.jaime@gmail.com>
2015-06-20 20:51:08 -0400

    #17 - now working with minified CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 var gulp = require('gulp');
 var debug = require('gulp-debug');
 var uglify = require('gulp-uglify');
-
+var concat = require('gulp-concat');
+var uglifyCss = require('gulp-uglifycss');
 
 gulp.task('uglify', function() {
   gulp.src([	 
-	  'src/main/resources/public/libraries/angular/angular.min.js',
-	   'src/main/resources/public/libraries/angular-ui-router/release/angular-ui-router.min.js',
 	  'src/main/resources/public/modules/*.js',
 	  'src/main/resources/public/modules/services/**/*.js',
 	  'src/main/resources/public/modules/directives/**/*.js',
@@ -14,12 +13,41 @@ gulp.task('uglify', function() {
 	])
 	.pipe(debug())
 	.pipe(uglify())
-    .pipe(gulp.dest('src/main/resources/public/'));
+	.pipe(concat('ctf-app-module.js'))
+	.pipe(gulp.dest('src/main/resources/public'));
 });
 
-gulp.task('concatenate', function() {
-	
-})
+gulp.task('concatenateJs', function() {
+	gulp.src([
+		'src/main/resources/public/libraries/angular/angular.min.js',
+		'src/main/resources/public/libraries/angular-ui-router/release/angular-ui-router.min.js',
+		'src/main/resources/public/ctf-app-module.js'
+	])
+	.pipe(debug())
+	.pipe(concat('ctf-app.js'))
+	.pipe(gulp.dest('src/main/resources/public'));
+});
 
-gulp.task('default', function() {
+gulp.task('uglifyCss', function() {
+  gulp.src([	 
+		'src/main/resources/public/styles/bridgephase.css'
+	])
+	.pipe(debug())
+	.pipe(uglifyCss())
+	.pipe(concat('bridgephase.min.css'))
+	.pipe(gulp.dest('src/main/resources/public/styles/'));
+});
+
+gulp.task('concatenateCss', ['uglifyCss'], function() {
+	gulp.src([
+		'src/main/resources/public/styles/pure-min.css',
+		'src/main/resources/public/styles/grids-responsive-min.css',
+		'src/main/resources/public/styles/bridgephase.min.css'
+	])
+	.pipe(debug())
+	.pipe(concat('ctf-styles.css'))
+	.pipe(gulp.dest('src/main/resources/public'));
+});
+
+gulp.task('default', ['uglify', 'concatenateJs', 'concatenateCss'], function() {
 });
