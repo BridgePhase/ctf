658b7a46db5569b69855a055c1a4e7464d3a8157
Kane Gyovai <kane.gyovai@gmail.com>
2015-06-21 09:46:02 -0400

    DOMAIN: JSON updates.

--- src/main/java/com/bridgephase/ctf/backend/domain/Meta.java
+++ src/main/java/com/bridgephase/ctf/backend/domain/Meta.java
@@ -3,11 +3,9 @@ package com.bridgephase.ctf.backend.domain;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonAutoDetect
 public class Meta {
 	
 	private String disclaimer;
