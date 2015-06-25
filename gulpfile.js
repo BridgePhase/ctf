6fdb612513a962913cbe4655b0ef2e2012cd8251
Jaime Garcia <garciar.jaime@gmail.com>
2015-06-25 17:44:31 -0400

    #6 - Removing unused dependencies from the UI, jQuery and Datatables

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,17 @@ var uglifyCss = require('gulp-uglifycss');
 var runSequence = require('run-sequence');
 var less = require('gulp-less');
 
+/**
+ * By default, we want to turn our LESS files into CSS, and concatenate them
+ * and then do the same for JavaScript (except the LESS part) 
+ */
+gulp.task('default', function() {
+	// this neat plugin lets us run tasks in sequences since we can't
+	// concatenate if we haven't uglified yet
+	runSequence('lessify', 'uglifyCss', 'concatenateCss');
+	runSequence('uglifyJs', 'concatenateJs');
+});
+
 gulp.task('uglifyJs', function() {
   return gulp.src([	 
 	  'src/main/resources/public/modules/*.js',
@@ -25,8 +36,6 @@ gulp.task('concatenateJs', function() {
 		'src/main/resources/public/libraries/angular-ui-router/release/angular-ui-router.min.js',
 		'src/main/resources/public/libraries/angular-touch/angular-touch.min.js',
 		'src/main/resources/public/libraries/angular-carousel/dist/angular-carousel.min.js',
-		'src/main/resources/public/libraries/jquery/dist/jquery.min.js',
-		'src/main/resources/public/libraries/DataTables/media/js/jquery.dataTables.min.js',
 		'src/main/resources/public/ctf-app-module.js'
 	])
 	.pipe(debug({title: 'concatenatejs'}))
@@ -57,16 +66,10 @@ gulp.task('concatenateCss', function() {
 	return gulp.src([
 		'src/main/resources/public/styles/pure-min.css',
 		'src/main/resources/public/styles/grids-responsive-min.css',
-		'src/main/resources/public/libraries/DataTables/media/css/jquery.dataTables.min.css',
 		'src/main/resources/public/libraries/angular-carousel/dist/angular-carousel.min.css',
 		'src/main/resources/public/styles/ctf.min.css'
 	])
 	.pipe(debug({title:'concatenateCss'}))
 	.pipe(concat('ctf-styles.css'))
 	.pipe(gulp.dest('src/main/resources/public'));
-});
-
-gulp.task('default', function() {
-	runSequence('lessify', 'uglifyCss', 'concatenateCss');
-	runSequence('uglifyJs', 'concatenateJs');
 });
