e7db40bc0a0d6dded7be47fa5b2d5502461af82e
Kane Gyovai <kane.gyovai@gmail.com>
2015-06-25 00:25:08 -0400

    #4 - Added notification methods. Unable to create transactional context within scheduled task.

--- src/main/java/com/bridgephase/ctf/backend/notifications/NotificationService.java
+++ src/main/java/com/bridgephase/ctf/backend/notifications/NotificationService.java
@@ -1,49 +1,42 @@
 package com.bridgephase.ctf.backend.notifications;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bridgephase.ctf.model.jpa.Notification;
+import com.bridgephase.ctf.model.repository.NotificationRepository;
 
-@Service
+@Service 
 public class NotificationService {
 	
 	private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
 	
-	/**
-	 * 
-	 * @return
-	 */
+	@Autowired
+	private NotificationRepository repository;
+	
+	@Transactional
 	public List<Notification> getNotifications() {
 		logger.info("Retreiving notificaitons.");
-		List<Notification> items = new ArrayList<>();
-		for (int i = 0; i < 6; i++) {
-			Notification item = new Notification();
-			item.setHeadline("Headline " + i);
-			items.add(item);
-		}
+		// TODO - Update result list with a limit or time threshold (e.g. 5 days ago).
+		List<Notification> items = repository.findAll();
+		logger.info(String.format("A total of %s notifications were located.", items.size()));
 		return items;
 	}
 	
-	/**
-	 * 
-	 */
+	@Transactional
 	public void updateNotifications() {
 		logger.info("Updating notifications.");
-		List<Notification> notifications = generateNotifications();
-		saveNotifications(notifications);
-	}
-	
-	private List<Notification> generateNotifications() {
-		List<Notification> notifications = new ArrayList<>();
-		return notifications;
-	}
-	
-	private void saveNotifications(List<Notification> notifications) {
-		logger.info("Saving notifications.");
+		for (int i = 0; i < 6; i++) {
+			Notification item = new Notification();
+			item.setHeadline("Headline " + i);
+			repository.save(item);
+		}
+		List<Notification> savedItems = repository.findAll();
+		logger.info(String.format("A total of %s notifications were located.", savedItems.size()));
 	}
 }
--- src/main/java/com/bridgephase/ctf/model/repository/NotificationRepository.java
+++ src/main/java/com/bridgephase/ctf/model/repository/NotificationRepository.java
@@ -1,9 +1,11 @@
 package com.bridgephase.ctf.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.bridgephase.ctf.model.jpa.Notification;
 
 public interface NotificationRepository extends CrudRepository<Notification, Long> {
-
+	List<Notification> findAll();
 }
