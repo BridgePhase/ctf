aa3bbbe78f832cccaf9c8e1008776a781df022a0
Jaime Garcia <garciar.jaime@gmail.com>
2015-06-28 13:08:41 -0400

    [MISC] - Adding unit tests

--- src/test/java/com/bridgephase/ctf/view/controller/HomeControllerTest.java
+++ src/test/java/com/bridgephase/ctf/view/controller/HomeControllerTest.java
@@ -10,10 +10,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 
 public class HomeControllerTest {
 
@@ -26,7 +28,7 @@ public class HomeControllerTest {
 	
 	@Test
 	public void homeReturnsCorrectBinding() {
-		assertEquals("home", controller.home(Mockito.mock(Model.class)));
+		assertEquals("home", controller.home(mock(Model.class)));
 	}
 
 	@Test
@@ -38,11 +40,33 @@ public class HomeControllerTest {
 	}
 	
 	@Test
-	public void aliveCanHandleAnExceptionReadingVersionFile() throws IOException {
+	public void aliveCanHandleAnExceptionResadingVersionFile() throws IOException {
 		BufferedReader fakeReader = mock(BufferedReader.class);
 		doThrow(new IOException()).when(fakeReader).readLine();
 		doReturn(fakeReader).when(controller).versionReader();
 		
 		assertEquals("No version information available", controller.alive());
+	}
+	
+	@Test
+	public void partialsReturnTheCorrectUrlForPartialTemplates() {
+		assertEquals("modules/mypartial", controller.partial(mock(Model.class), "mypartial"));
+	}
+	
+	@Test
+	public void errorHandlerProvidesCorrectInformation() throws Exception {
+		BufferedReader fakeReader = new BufferedReader(new StringReader("fake-version-junit"));
+		doReturn(fakeReader).when(controller).versionReader();
+		HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+		doReturn(new StringBuffer("http://notarealserver.com")).when(mockRequest).getRequestURL();
+
+		ModelAndView result = controller.handleError(mockRequest);
+		
+		assertEquals("fake-version-junit", result.getModel().get("version"));
+	}
+	
+	@Test
+	public void correctErrorPath() {
+		assertEquals("/errorpage", controller.getErrorPath());
 	}
 }
