6fe962acc35e7037cffcbdb6d6d75c6ca3701320
Kane Gyovai <kane.gyovai@gmail.com>
2015-06-28 21:47:21 -0400

    [MISC] Updated unit test for recall classifications. Code coverage = 100%

--- src/test/java/com/bridgephase/ctf/backend/domain/enumeration/RecallClassificationTest.java
+++ src/test/java/com/bridgephase/ctf/backend/domain/enumeration/RecallClassificationTest.java
@@ -17,10 +17,26 @@ public class RecallClassificationTest {
 	@Test
 	public void valueOf() {
 		assertEquals(RecallClassification.CLASS_I, RecallClassification.valueOf("CLASS_I"));
+		assertEquals(RecallClassification.CLASS_II, RecallClassification.valueOf("CLASS_II"));
+		assertEquals(RecallClassification.CLASS_III, RecallClassification.valueOf("CLASS_III"));
+	}
+	
+	@Test
+	public void correctDescriptions() {
+		assertEquals(RecallClassification.CLASS_I.description(), "dangerous or defective");
+		assertEquals(RecallClassification.CLASS_II.description(), "to pose only a slight threat of serious nature");
+		assertEquals(RecallClassification.CLASS_III.description(), "unlikely to cause any adverse health reaction");
 	}
 	
 	@Test
 	public void parseHandlesProblems() {
 		assertNull(RecallClassification.parse("Not A real Key"));
+	}
+	
+	@Test
+	public void testParse() {
+		for (RecallClassification value : RecallClassification.values()) {
+			assertEquals(value, RecallClassification.parse(value.label()));
+		}
 	}
 }
