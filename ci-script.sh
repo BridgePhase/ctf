126b32a69ba467df752ae8fb7a4d1c5f234b79f7
Jaime Garcia <garciar.jaime@gmail.com>
2015-06-29 22:41:43 -0400

    [MISC] - added logging

--- ci-script.sh
+++ ci-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-./gradlew build awsRevision 
+./gradlew build awsRevision
+echo "Starting server now" 
 java -jar build/libs/ctf-1.0.jar
+sleep 5
 echo "Running protractor tests now"
 protractor e2e/configuration.js
