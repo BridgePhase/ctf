080c63e30d876ac06b45f151ddda3ef44e049fd7
Jaime Garcia <garciar.jaime@gmail.com>
2015-06-29 22:17:02 -0400

    [MISC] - testing with firefox

--- ci-script.sh
+++ ci-script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ./gradlew build awsRevision
-webdriver-manager start &
 ./gradlew bootRun &
 protractor e2e/configuration.js
--- e2e/configuration.js
+++ e2e/configuration.js
@@ -2,6 +2,6 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['*.test.js'],
   capabilities: {
-    browserName: 'phantomjs'
+    browserName: 'firefox'
   }
 };
