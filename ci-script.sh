c19137b77053aade4c51c1f5f9de3d3255494150
Jaime Garcia <garciar.jaime@gmail.com>
2015-06-29 22:45:20 -0400

    [MISC] - now with bootRun

--- ci-script.sh
+++ ci-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./gradlew build awsRevision
 echo "Starting server now" 
-java -jar build/libs/ctf-1.0.jar
+./gradlew bootRun &
 sleep 5
 echo "Running protractor tests now"
 protractor e2e/configuration.js
