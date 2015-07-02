667c89547f0196c15f8d5cd1e12befdfc290282a
Jaime Garcia <garciar.jaime@gmail.com>
2015-07-01 21:19:03 -0400

    Full deployment

--- awsscripts/start-docker.sh
+++ awsscripts/start-docker.sh
@@ -2,4 +2,4 @@
 echo "Building image now"
 sudo docker build -t ctf-tomcat /home/ubuntu/docker
 echo "Image built"
-#sudo docker run -d --name ctf-deployment -ti -p 8080:8080 ctf-tomcat
+sudo docker run -d --name ctf-deployment -ti -p 8080:8080 ctf-tomcat
