e3c83686bcc92d7775568c33136bf2bf8d065fab
Jaime Garcia <garciar.jaime@gmail.com>
2015-07-02 18:03:39 -0400

    [DOCKER] - Build a docker image

--- ci-script.sh
+++ ci-script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ./gradlew clean build awsRevision
-#echo "Starting server now" 
-#export production=true
-#./gradlew bootRun & >/dev/null 2>&1
-#echo "Waiting for server now"
-#sleep 30
-#echo "Running protractor tests now"
-#protractor e2e/configuration.js
+echo "Starting server now" 
+export production=true
+java -jar build/libs/ctf-1.0.jar & >/dev/null 2>&1
+echo "Waiting for server now"
+sleep 30
+echo "Running protractor tests now"
+protractor e2e/configuration.js
