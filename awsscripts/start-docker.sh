ef6c837f09e53724c51fcb553154147715215cdd
Jaime Garcia <garciar.jaime@gmail.com>
2015-07-01 21:02:51 -0400

    Testing docker deploy again

--- awsscripts/start-docker.sh
+++ awsscripts/start-docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "Building image now"
 sudo docker build -t ctf-tomcat .
-sudo docker run -d --name ctf-deployment -ti -p 8080:8080 ctf-tomcat
+echo "Image built"
+#sudo docker run -d --name ctf-deployment -ti -p 8080:8080 ctf-tomcat
