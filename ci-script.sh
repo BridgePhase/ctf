5f269411b114e95afdffc54d2da5ff96bb8394e5
Jaime Garcia <garciar.jaime@gmail.com>
2015-07-02 22:00:59 -0400

    [MISC] docker deployment

--- ci-script.sh
+++ ci-script.sh
@@ -2,6 +2,8 @@
 ./gradlew clean build awsRevision dockerRevision
 echo "Starting server now" 
 export production=true
+export cron_notifications="0 5,17 * * * *"
+export cron_cache_clear="0 5,17 * * * *"
 java -jar build/libs/ctf-1.0.jar & >/dev/null 2>&1
 echo "Waiting for server now"
 sleep 15
