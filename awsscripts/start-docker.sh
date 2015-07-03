aafb6491f03c007ebdfb28daa42a5ff416b9bf6d
Jaime Garcia <garciar.jaime@gmail.com>
2015-07-02 21:55:03 -0400

    [MISC] - Updated docker deployment

--- awsscripts/start-docker.sh
+++ awsscripts/start-docker.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo "Building image now"
-sudo docker build -t ctf-tomcat /home/ubuntu/docker
-echo "Image built"
 # We run this in detached mode so that it doesn't hang code deploy
-sudo docker run -d --name ctf-deployment -ti -p 8080:8080 ctf-tomcat
+sudo docker run -d --name ctf-deployment -ti -p 8080:8080 jramirez/bridgephase:_TAG_
