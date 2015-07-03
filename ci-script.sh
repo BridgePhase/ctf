3cdeb8241240b1779463e207c5c46680b672f69e
Jaime Garcia <garciar.jaime@gmail.com>
2015-07-02 20:28:43 -0400

    [MISC] - Build docker image

--- ci-script.sh
+++ ci-script.sh
@@ -4,6 +4,6 @@ echo "Starting server now"
 export production=true
 java -jar build/libs/ctf-1.0.jar & >/dev/null 2>&1
 echo "Waiting for server now"
-sleep 30
+sleep 15
 echo "Running protractor tests now"
 protractor e2e/configuration.js
